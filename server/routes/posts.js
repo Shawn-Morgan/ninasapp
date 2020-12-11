566f7774be11fefd1aeb8fbc1c5546f95f3d57f9
shawnm7474 <shawnm74@gmail.com>
2020-12-10 21:46:05 -0600

    foundation front-end and back-end - created mongodb cluster

---
+++ server/routes/posts.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+
+router.get('/:id', getPost);
+
+router.post('/', createPost);
+//update
+//delete
+//like
+
+export default router;
