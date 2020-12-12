2f48318ac1f1ee9ee79a65bbd15bab593dee20fe
shawnm7474 <shawnm74@gmail.com>
2020-12-12 14:34:45 -0600

    more work on connecting the front-end to the back-end

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,13 +1,18 @@
 import React from 'react';
+// import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/Post';
-
-
 import useStyles from './styles';
 
 //need to add className here next to <h1>
 
 const Posts = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+
+    console/log(posts);
+
     return (
         <>
             
